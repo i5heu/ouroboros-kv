a668292d03f453df7875aeb3b19ef1070a5b70fe
i5heu <i5heu@i5h.eu>
2025-08-08 11:19:20 +0000

    refactor: Simplify benchmark tests by removing redundant keyCount variable and improving data handling

--- benchmark_test.go
+++ benchmark_test.go
@@ -11,29 +11,26 @@ import (
 )
 
 func BenchmarkWriteReadNavigation(b *testing.B) {
-	const (
-		keyCount  = 100
-		valueSize = 4096
-	)
-
-	// Generate random data and keys
-	values := make([][]byte, keyCount)
-	dataObjs := make([]Data, keyCount)
-	for i := 0; i < keyCount; i++ {
-		values[i] = make([]byte, valueSize)
-		rand.Read(values[i])
-		key := hash.HashBytes(values[i])
-		dataObjs[i] = Data{
-			Key:                     key,
-			Content:                 values[i],
-			Parent:                  hash.Hash{},
-			Children:                []hash.Hash{},
-			ReedSolomonShards:       2,
-			ReedSolomonParityShards: 1,
-		}
-	}
+	const valueSize = 4096
 
 	b.Run("WriteData", func(b *testing.B) {
+		keyCount := b.N
+		values := make([][]byte, keyCount)
+		dataObjs := make([]Data, keyCount)
+		for i := 0; i < keyCount; i++ {
+			values[i] = make([]byte, valueSize)
+			rand.Read(values[i])
+			key := hash.HashBytes(values[i])
+			dataObjs[i] = Data{
+				Key:                     key,
+				Content:                 values[i],
+				Parent:                  hash.Hash{},
+				Children:                []hash.Hash{},
+				ReedSolomonShards:       2,
+				ReedSolomonParityShards: 1,
+			}
+		}
+
 		dir := b.TempDir()
 		cryptInstance := crypt.New()
 		config := &Config{
@@ -48,13 +45,29 @@ func BenchmarkWriteReadNavigation(b *testing.B) {
 
 		b.ResetTimer()
 		for n := 0; n < b.N; n++ {
-			i := n % keyCount
-			err := kv.WriteData(dataObjs[i])
+			err := kv.WriteData(dataObjs[n])
 			require.NoError(b, err)
 		}
 	})
 
 	b.Run("ReadData", func(b *testing.B) {
+		keyCount := b.N
+		values := make([][]byte, keyCount)
+		dataObjs := make([]Data, keyCount)
+		for i := 0; i < keyCount; i++ {
+			values[i] = make([]byte, valueSize)
+			rand.Read(values[i])
+			key := hash.HashBytes(values[i])
+			dataObjs[i] = Data{
+				Key:                     key,
+				Content:                 values[i],
+				Parent:                  hash.Hash{},
+				Children:                []hash.Hash{},
+				ReedSolomonShards:       2,
+				ReedSolomonParityShards: 1,
+			}
+		}
+
 		dir := b.TempDir()
 		cryptInstance := crypt.New()
 		config := &Config{
@@ -75,14 +88,30 @@ func BenchmarkWriteReadNavigation(b *testing.B) {
 
 		b.ResetTimer()
 		for n := 0; n < b.N; n++ {
-			i := n % keyCount
-			read, err := kv.ReadData(dataObjs[i].Key)
+			read, err := kv.ReadData(dataObjs[n].Key)
 			require.NoError(b, err)
-			require.Equal(b, dataObjs[i].Content, read.Content)
+			require.Equal(b, dataObjs[n].Content, read.Content)
 		}
 	})
 
 	b.Run("ListStoredData", func(b *testing.B) {
+		keyCount := b.N
+		values := make([][]byte, keyCount)
+		dataObjs := make([]Data, keyCount)
+		for i := 0; i < keyCount; i++ {
+			values[i] = make([]byte, valueSize)
+			rand.Read(values[i])
+			key := hash.HashBytes(values[i])
+			dataObjs[i] = Data{
+				Key:                     key,
+				Content:                 values[i],
+				Parent:                  hash.Hash{},
+				Children:                []hash.Hash{},
+				ReedSolomonShards:       2,
+				ReedSolomonParityShards: 1,
+			}
+		}
+
 		dir := b.TempDir()
 		cryptInstance := crypt.New()
 		config := &Config{
